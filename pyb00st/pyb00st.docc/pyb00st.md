7360b7327a2d1d59b116ec874a4d301b3426c462
Egor Merkushev <egor.merkushev@corp.vk.com>
2024-01-23 23:09:52 +0300

    Create Xcode project with tests and docc

---
+++ pyb00st/pyb00st.docc/pyb00st.md
@@ -0,0 +1,13 @@
+# ``pyb00st``
+
+Port of pyb00st library for LEGO® BOOST on Swift for iOS & macOS
+
+## Overview
+
+Original repository of [Python library](https://github.com/JorgePe/pyb00st).
+
+## Quick Start
+
+### Installation
+
+- Install framework as Swift Package in your Xcode project of workspace. 
---
+++ pyb00st/pyb00st.h
@@ -0,0 +1,18 @@
+//
+//  pyb00st.h
+//  pyb00st
+//
+//  Created by Egor Merkushev on 23.01.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for pyb00st.
+FOUNDATION_EXPORT double pyb00stVersionNumber;
+
+//! Project version string for pyb00st.
+FOUNDATION_EXPORT const unsigned char pyb00stVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <pyb00st/PublicHeader.h>
+
+
